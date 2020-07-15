857f6b8b0afa9a36f1de9c291bb6348a379edd97
Volha Zakharava <zakharava.volha@gmail.com>
2020-07-15 22:10:00 +0300

    feat: add  packages to package json, add webpack config, and ts config, install nuget packages for dealing with react.

---
+++ JobbBok/src/JobbBok.Web/ClientApp/src/components/app/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles/index.css';
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello Jobb bok!!</h1>
+    </div>
+  );
+};
--- JobbBok/src/JobbBok.Web/Startup.cs
+++ JobbBok/src/JobbBok.Web/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -26,9 +27,14 @@ namespace JobbBok.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the r untime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -45,6 +51,17 @@ namespace JobbBok.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start"); 
+                }
             });
         }
     }
